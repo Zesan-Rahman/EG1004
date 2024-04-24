bc21ed8e07b3462ebe9618d9ad9f006e1204b127
Zesan Rahman <zrahman30@stuy.edu>
2024-04-24 14:24:24 -0400

    Using PaddleOCR and implemented img rotation

--- Reading/cameraStuff.py
+++ Reading/cameraStuff.py
@@ -1,12 +1,27 @@
-from picamera2 import Picamera2, Preview
-import time
+from paddleocr import PaddleOCR, draw_ocr
+from PIL import Image
 
-picam2 = Picamera2()
-#setup configuration, highres = 1920x1080, minimum res = 640x480
-camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
-#configure camera
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+def flatten(items, seqtypes=(list, tuple)):
+    for i, x in enumerate(items):
+        while i < len(items) and isinstance(items[i], seqtypes):
+            items[i:i+1] = items[i]
+    return items
+
+def getName(result):
+    names = []
+    for item in result:
+        if(isinstance(item, str)):
+            names.append(item)
+    return names
+
+def rotate(fileName, degree):
+    originalImg = Image.open(fileName)
+    rotatedImg = originalImg.rotate(degree)
+    rotatedImg.save("Rotated.jpg")
+
+ocr = PaddleOCR(use_angle_cls=True, lang='en', use_gpu=False)
+result = ocr.ocr("derrick.jpg")
+rotate("derrick.jpg", 90)
+result = flatten(result)
+print(result)
+print(getName(result))
